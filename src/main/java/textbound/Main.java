86ba52640299853cdc19250f40c03c0c9fa1b004
S-learnss <s23mlomzale@gmail.com>
2026-08-14 22:25:59 +0200

    Add Room class

--- src/main/java/textbound/Main.java
+++ src/main/java/textbound/Main.java
@@ -4,8 +4,12 @@ public class Main {
 
     public static void main(String[] args) {
 
-        Game game = new Game();
-        game.start();
+        Room village = new Room(
+            "Town of Beginnings",
+            "The blackness which floods your psyche gradually splits open, blue flooding yours eyes as numerous crowds of adventurers move about the village square. The smell of fresh bread and the sound of a blacksmith's hammer fill the air, as you take in your surroundings."
+        );
 
+        System.out.println(village.getName());
+        System.out.println(village.getDescription());
     }
 }
